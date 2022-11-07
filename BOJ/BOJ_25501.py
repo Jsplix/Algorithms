35c5000e9ffc34caac55be4f6e0863a11fe36448
Jsplix <jkjin112@gmail.com>
2022-11-07 09:18:07 +0900

    Recursion

---
+++ BOJ/BOJ_25501.py
@@ -0,0 +1,20 @@
+# BOJ_25501_재귀의 귀재 / Recursion
+import sys
+input = sys.stdin.readline
+
+def isPalindrome(s, l, r):
+    global call_time
+    call_time += 1
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: 
+        if isPalindrome(s, l + 1, r - 1): return 1
+        else: return 0
+
+n = int(input())
+call_time = 0
+
+for _ in range(n):
+    s = input().rstrip()
+    call_time = 0
+    print(isPalindrome(s, 0, len(s) - 1), call_time)
