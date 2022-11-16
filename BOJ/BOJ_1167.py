4b4fbe8be35969d6077e20fcd75895a8c46d9ee7
Jsplix <jkjin112@gmail.com>
2022-11-17 01:01:24 +0900

    [BOJ] 트리의 지름 - Tree, DFS

--- BOJ/BOJ_1167.py
+++ BOJ/BOJ_1167.py
@@ -2,6 +2,9 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 # keypoint는 1에서부터의 최대 거리와 최대 거리를 가진 곳부터 다시 최대거리를(총 2번) 구하는 것.
+# r1, r2를 -1로 초기화해주는 이유 -> 0으로 해줄 경우 최대 거리를 가진 곳에서
+# DFS를 처리하면 본인으로 되돌아오게 됨 (그 때가 당연히 최대가 됨)
+# -1로 초기화해주고 시작지점을 0으로하면 검사할 때 -1이 아니므로 본인으로 돌아가지 않음.
 def dfs(idx, r):
     for v, d in graph[idx]:
         if r[v] == -1:
