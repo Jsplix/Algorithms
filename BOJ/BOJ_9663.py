ab6670d330e788cef4a32a6c886088ef317f764f
Jsplix <115134208+Jsplix@users.noreply.github.com>
2022-11-23 11:12:02 +0900

    [BOJ] 9663 N-Queen / 백트래킹

--- BOJ/BOJ_9663.py
+++ BOJ/BOJ_9663.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 def is_promising(x):
     for i in range(x):
         if row[x] == row[i] or abs(row[x] - row[i]) == x - i:
+            # 대각선 확인 -> (행 - 행) == (열 - 열)
             return False
     return True
 
@@ -12,13 +13,13 @@ def back(x):
     if x == n:
         cnt += 1
     else:
-        for i in range(n):
+        for i in range(n): # i는 열번호
             row[x] = i
             if is_promising(x):
                 back(x + 1)
 
 n = int(input())
 cnt = 0
-row = [0] * n
+row = [0] * n # Queen의 위치
 back(0)
 print(cnt)
