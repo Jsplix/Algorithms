204d2765d92afa27cb0bd320c4ad44ad59223546
Jsplix <jkjin112@gmail.com>
2022-12-18 02:05:42 +0900

    [BOJ] 14241 슬라임 합치기 / 수학

--- workspace/t1.py
+++ workspace/t1.py
@@ -1,2 +1,16 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
+# [BOJ] 14241 슬라임 합치기 / 수학
+n = int(input())
+slime_size = sorted(list(map(int, input().split())))
+score = 0
+
+while len(slime_size) >= 2:
+    x = slime_size.pop()
+    y = slime_size.pop()
+    score += x * y
+    slime_size.append(x + y)   
+    slime_size.sort() 
+    
+print(score)
