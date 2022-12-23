ac315ccd1eb91bc31dc668fabe40ca8b5098917c
Jsplix <115134208+Jsplix@users.noreply.github.com>
2022-12-23 10:55:53 +0900

    [BOJ] 7868 해밍 수열 / 브루트 포스, 정수론

--- BOJ/BOJ_7868.py
+++ BOJ/BOJ_7868.py
@@ -4,6 +4,9 @@ input = sys.stdin.readline
 p1, p2, p3, idx = map(int, input().split())
 arr = [p1, p2, p3]
 ans = []
+# 2**60제곱이 문제에서의 입력 제한인 10**18보다 더 큼
+# i, j, k는 각각 지수를 나타냄 -> 2**60이 10**18보다 더 크다 
+# ==> p1, p2, p3가 3 이상인 값들은 60보다 작은 제곱수여도 충분하다.
 for i in range(60):
     for j in range(60):
         for k in range(60):
