f21e3199f01da53404cb18eb9631b8c56acd5b1b
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-01-02 17:32:55 +0900

    [BOJ] 14003 가장 긴 증가하는 부분 수열 5 / 이분 탐색

--- BOJ/BOJ_14003.py
+++ BOJ/BOJ_14003.py
@@ -15,9 +15,13 @@ for i in range(n):
         idx[i] = len(chk) - 1
     else:
         idx[i] = bisect_left(chk, arr[i])
+        # bisect는 정렬된 배열(리스트)에서 이분 탐색을 진행힘.
+        # left는 해당 배열에서 값보다 작거나 같은 가장 큰 index를 반환하고
+        # right는 해당 배열에서 값보다 크거나 같은 가장 작은 index를 반환한다.
         chk[idx[i]] = arr[i]
 
 print(len(chk) - 1)
+# print(chk)
 
 max_idx = max(idx) + 1
 ans = []
