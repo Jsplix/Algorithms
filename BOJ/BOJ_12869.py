35022dc4d7b90e675bb1089086b148e1145da2be
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-01-06 00:13:23 +0900

    [BOJ] 12869 뮤탈리스크 / DP

---
+++ BOJ/BOJ_12869.py
@@ -0,0 +1,37 @@
+from itertools import permutations
+import sys
+input = sys.stdin.readline
+# [BOJ] 12869 뮤탈리스크 / DP
+def solve(a, b, c, cnt):
+    global ans
+    if a <= 0 and b <= 0 and c <= 0:
+        if ans > cnt:
+            ans = cnt
+            return
+    
+    if a <= 0: a = 0
+    if b <= 0: b = 0
+    if c <= 0: c = 0
+
+    if dp[a][b][c] <= cnt and dp[a][b][c] != 0:
+        return
+    
+    dp[a][b][c] = cnt
+
+    for i in permutations(damage, 3):
+        solve(a - i[0], b - i[1], c - i[2], cnt + 1)
+
+damage = [9, 3, 1]
+
+n = int(input())
+hp = list(map(int, input().split()))
+dp = [[[0 for _ in range(61)] for _ in range(61)] for _ in range(61)]
+ans = 100
+
+if n == 2:
+    hp.append(0)
+elif n == 1:
+    hp.append(0)
+    hp.append(0)
+solve(hp[0], hp[1], hp[2], 0)
+print(ans)
