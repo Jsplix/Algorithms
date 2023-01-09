7411ef040ceccec2bc748ccfe9c8f53ae1f7df88
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-01-09 15:19:11 +0900

    # [BOJ] 4811 알약 / DP

---
+++ BOJ/BOJ_4811.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+dp = [[0 for _ in range(31)] for _ in range(31)]
+for i in range(31):
+    for j in range(31):
+        if i > j: continue
+        if i == 0: dp[j][i] = 1
+        else: dp[j][i] = dp[j-1][i] + dp[j][i-1]
+
+for _ in range(1000):
+    n = int(input())
+    if n == 0: break
+    print(dp[n][n])
