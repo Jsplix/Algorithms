3f65357772e1d9495d5e704b629a833fef197bc0
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-01-12 10:05:36 +0900

    [BOJ] 1269 대칭 차집합 / 자료 구조, 집합

--- BOJ/BOJ_1269.py
+++ BOJ/BOJ_1269.py
@@ -1,7 +1,6 @@
-# BOJ_1269_대칭 차집합
 import sys
 input = sys.stdin.readline
-
+# [BOJ] 1269 대칭 차집합 / 자료 구조, 집합
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
