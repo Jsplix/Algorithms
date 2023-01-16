a72c361327794028009ada65cf9eef21c5e9d93f
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-01-16 17:51:05 +0900

    [BOJ] 1924 2007년 / 수학, 구현

--- BOJ/BOJ_1924.py
+++ BOJ/BOJ_1924.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-
+# [BOJ] 1924 2007년 / 수학, 구현
 x, y = map(int, input().split())
 calendar = { 1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31 }
 day = { 0: "MON", 1: "TUE", 2: "WED", 3: "THU", 4: "FRI", 5: "SAT", 6: "SUN" }
