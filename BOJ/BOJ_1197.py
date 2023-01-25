47ad44cbc33c3c92f4bb98ebe3d8207f4df24c80
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-01-25 10:41:41 +0900

    [BOJ] 1197 최소 스패닝 트리 / 최소 신장 트리, 그래프 이론, 크루스칼

--- BOJ/BOJ_1197.py
+++ BOJ/BOJ_1197.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-# [BOJ] 1197 최소 스패닝 트리 / 최소 신장 트리(MST), 그래프 이론, 크루스칼
+# [BOJ] 1197 최소 스패닝 트리 / 최소 신장 트리(MST), 그래프 이론, 크루스칼, 유니온-파인드
 
 v, e = map(int, input().split())
 edges = []
@@ -20,9 +20,10 @@ def find(x):
 val = 0
 
 for n1, n2, w in edges:
-    root_1 = find(n1)
-    root_2 = find(n2)
+    root_1 = find(n1) # n1의 부모 노드를 찾음
+    root_2 = find(n2) # n2의 부모 노드를 찾음
     if root_1 != root_2:
+        # 부모 노드의 값이 더 작은 쪽으로 합침
         if root_1 > root_2: vertex[root_1] = root_2
         else: vertex[root_2] = root_1
         val += w
