3c80c2a02ee6608fe44ba72def0518fbbcdd0f10
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-02-15 10:42:35 +0900

    [BOJ] 17427 약수의 합 2 / 수학, 정수론

---
+++ BOJ/BOJ_17427.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+# [BOJ] 17427 약수의 합 2 / 수학, 정수론
+n = int(input())
+answer = 0
+for x in range(1, n+1):
+    answer += (n//x)*x
+print(answer)
+# [BOJ] 1676 과 동일한 메커니즘. n / i (i는 자연수) 는 n에서 i의 약수의 개수는 n/i이다.
