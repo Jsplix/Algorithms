669c18097db28a048f90d574c11a1013d1cb296c
Jsplix <jkjin112@gmail.com>
2023-02-22 19:38:48 +0900

    [BOJ] 27496 발머의 피크 이론 / 누적 합, 슬라이딩 윈도우

--- BOJ/BOJ_27496.py
+++ BOJ/BOJ_27496.py
@@ -5,7 +5,7 @@ n, l = map(int, input().split())
 alcohol = list(map(int, input().split()))
 blc = 0
 count, idx = 0, 0
-for i in range(l):
+for i in range(min(n, l)): # 기존의 코드는 n이 l보다 작을 경우 indexError 발생
     blc += alcohol[i]
     if 129 <= blc <= 138: count += 1
 
