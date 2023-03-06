9f10561be5b7fd1522f1e73827c8eb56bc74bd4e
Jsplix <jkjin112@gmail.com>
2023-03-06 23:56:14 +0900

    studying py

--- Studying/naver_boostcourse/study.py
+++
@@ -1,5 +0,0 @@
-import fah_converter as fah
-print("Enter a celsius value: ")
-celsius = float(input())
-fahrenheit = fah.convert_c_to_f(celsius)
-print("That's ", fahrenheit, " degrees Fahrenheit")
