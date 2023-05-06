86f77288543da7360b2a592ed178a5a2c9829fca
Jsplix <jkjin112@gmail.com>
2023-05-06 22:04:26 +0900

    [BOJ] 11568 민균이의 계략 / DP, LIS

---
+++ BOJ/BOJ_11568.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+# [BOJ] 11568 민균이의 계략 / DP, LIS
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
