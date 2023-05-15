56db0ac20287f3e3d5a15e2aed2504e82798b523
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-05-15 20:12:20 +0900

    delete commit

--- Studying/naver_boostcourse/fah_converter.py
+++
@@ -1,8 +0,0 @@
-def convert_c_to_f(celcius_value):
-    return celcius_value * 9.0 / 5 + 32
-
-if __name__ == '__main__':
-    test = "GGG"
-    print(test)
-# import로 호출시 모듈의 모든 내용을 호출하게 됨 -> test라는 내용도 자동 호출되기 때문에 출력이 되는 것임
-# 방지하기 위해서 if __name__ == '__main__':를 사용하면 됨
