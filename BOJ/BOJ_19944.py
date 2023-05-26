ddc494006c8c6f1cdcd1f880f08d5de0983dfbc7
Jsplix <115134208+Jsplix@users.noreply.github.com>
2023-05-26 21:58:16 +0900

    [BOJ] 19944 뉴비의 기준은 뭘까? / 구현

--- BOJ/BOJ_19944.py
+++ BOJ/BOJ_19944.py
@@ -1,13 +1,5 @@
 n, m = map(int, input().split())
 # [BOJ] 19944 뉴비의 기준은 뭘까? / 구현
-if m == 1 or m == 2:
-
-    print("NEWBIE!")
-
-elif m <= n and m != 1 and m != 2:
-
-    print("OLDBIE!")
-
-else:
-
-    print("TLE!")
+if m == 1 or m == 2: print("NEWBIE!")
+elif m <= n and m != 1 and m != 2: print("OLDBIE!")
+else: print("TLE!")
