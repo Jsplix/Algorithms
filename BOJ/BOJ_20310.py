800ddd781e8d8a6435490fdd917c09f3950d952a
Jsplix <jkjin112@gmail.com>
2023-06-26 00:01:19 +0900

    [BOJ] 20310 타노스 / 문자열, 그리디

--- BOJ/BOJ_20310.py
+++ BOJ/BOJ_20310.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 # [BOJ] 20310 타노스 / 문자열, 그리디
+# 핵심: 순서 유지, 사전순 정렬 - 0이 최대한 앞에 있어야 하므로 뒤에서부터 0 제거, 1은 앞에서부터 제거
 s = list(input().rstrip())
 zero_cnt = s.count('0') // 2
 one_cnt = s.count('1') // 2
