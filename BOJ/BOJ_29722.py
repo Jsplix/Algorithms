ce5a7e95c55766eaab6e5b7315c8e0f732c3e2a4
Jsplix <jkjin112@gmail.com>
2023-09-24 23:59:59 +0900

    asd

---
+++ BOJ/BOJ_29722.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+year, month, day = map(int, input().split('-'))
+n = int(input())
+
+py = year + (n // 360); n %= 360
+pm = month + (n // 30); n %= 30
+pd = day + n
+
+if pd > 30: pd %= 30; pm += 1
+if pm > 12: pm %= 12; py += 1
+print(py, str(pm).zfill(2), str(pd).zfill(2), sep='-')
