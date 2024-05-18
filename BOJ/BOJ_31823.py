0cbda5cfee87d85f538892c371b0da409117eb91
Jsplix <jkjin112@gmail.com>
2024-05-19 00:33:52 +0900

    [BOJ] 31823 악질 검거 / 구현

--- BOJ/BOJ_31823.py
+++ BOJ/BOJ_31823.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-
+# [BOJ] 31823 악질 검거 / 구현
 n, m = map(int, input().split())
 streak, who = [], []
 
@@ -12,13 +12,20 @@ for _ in range(n):
 answer = []
 for i in range(n):
     chk = 0
+    mx = 0
     flag = False
     for j in range(m):
         if streak[i][j] == '.':
             if flag: chk += 1
             else: chk = 1 ; flag = True
-        else: flag = False
-    answer.append(chk)
+        else: 
+            if chk:
+                mx = max(mx, chk)
+                chk = 0
+            flag = False
+    mx = max(mx, chk)
+
+    answer.append(mx)
 
 print(len(set(answer)))
 for sr, nm in zip(answer, who):
