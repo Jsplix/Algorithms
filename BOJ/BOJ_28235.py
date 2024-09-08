d691deb28a2cb560ea957f8a6819cdea91f749c5
Jsplix <jkjin112@gmail.com>
2024-09-08 23:04:32 +0900

    [BOJ] 28235 코드마스터 2023 / 구현

---
+++ BOJ/BOJ_28235.py
@@ -0,0 +1,5 @@
+import sys
+input = sys.stdin.readline
+# [BOJ] 28235 코드마스터 2023 / 구현
+response = {"SONGDO": "HIGHSCHOOL", "CODE": "MASTER", "2023": "0611", "ALGORITHM": "CONTEST"}
+print(response[input().rstrip()])
