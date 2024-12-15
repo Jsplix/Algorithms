eb5f96a4108b5da0ffea37518a200b6d8e0ad42d
Jsplix <jkjin112@gmail.com>
2024-12-15 23:43:11 +0900

    [BOJ] 2393 Rook / 구현

---
+++ BOJ/BOJ_2393.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+# [BOJ] 2393 Rook / 구현
+print("  ___  ___  ___")
+print("  | |__| |__| |")
+print("  |           |")
+print("   \_________/")
+print("    \_______/")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |_____|")
+print("  __/       \__")
+print(" /             \\")
+print("/_______________\\")
